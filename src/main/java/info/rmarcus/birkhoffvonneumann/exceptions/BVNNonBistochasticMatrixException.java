d3d7d07c5c2863affe490179711e87e65cbb9d67
Ryan Marcus <ryan@ryanmarc.us>
2017-01-11 15:26:53 -0800

    bumped version, updated license

--- src/main/java/info/rmarcus/birkhoffvonneumann/exceptions/BVNNonBistochasticMatrixException.java
+++ src/main/java/info/rmarcus/birkhoffvonneumann/exceptions/BVNNonBistochasticMatrixException.java
@@ -1,41 +1,23 @@
 // < begin copyright > 
-// Copyright Ryan Marcus 2016
+// Copyright Ryan Marcus 2017
 // 
-// This file is part of brikhoffvonneumann.
+// This file is part of birkhoffvonneumann.
 // 
-// brikhoffvonneumann is free software: you can redistribute it and/or modify
+// birkhoffvonneumann is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 // 
-// brikhoffvonneumann is distributed in the hope that it will be useful,
+// birkhoffvonneumann is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 // 
 // You should have received a copy of the GNU General Public License
-// along with brikhoffvonneumann.  If not, see <http://www.gnu.org/licenses/>.
+// along with birkhoffvonneumann.  If not, see <http://www.gnu.org/licenses/>.
 // 
 // < end copyright > 
-// < begin copyright > 
-// Copyright Ryan Marcus 2016
-// 
-// This file is part of brikhoffvonneumann.
-// 
-// brikhoffvonneumann is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// brikhoffvonneumann is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with brikhoffvonneumann.  If not, see <http://www.gnu.org/licenses/>.
-// 
-// < end copyright > 
+ 
 package info.rmarcus.birkhoffvonneumann.exceptions;
 
 public class BVNNonBistochasticMatrixException extends BVNException {
